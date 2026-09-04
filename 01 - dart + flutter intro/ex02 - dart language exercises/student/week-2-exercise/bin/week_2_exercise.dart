00596b3279f15a5f69202d87be8373b76aa20039
Juan Lugo <jlugohernandez1@nait.ca>
2026-09-04 11:33:52 -0600

    Added 1st exercise to week 2

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/week-2-exercise/bin/week_2_exercise.dart
@@ -0,0 +1,5 @@
+import 'package:week_2_exercise/week_2_exercise.dart' as week_2_exercise;
+
+void main(List<String> arguments) {
+  print('Hello world: ${week_2_exercise.calculate()}!');
+}
---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/week-2-exercise/test/week_2_exercise_test.dart
@@ -0,0 +1,8 @@
+import 'package:week_2_exercise/week_2_exercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
