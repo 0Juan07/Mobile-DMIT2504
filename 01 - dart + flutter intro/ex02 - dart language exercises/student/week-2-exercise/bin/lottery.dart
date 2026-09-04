97136bd8db40c69b62ef0c0e53e161ee18627291
Juan Lugo <jlugohernandez1@nait.ca>
2026-09-04 11:39:50 -0600

    Added arrays initialization for the proj

---
+++ 01 - dart + flutter intro/ex02 - dart language exercises/student/week-2-exercise/bin/lottery.dart
@@ -0,0 +1,4 @@
+// 1. Program gonna run by itself, --> main
+// 2. Handle command-line user inputs
+// 3. Any function that returns a list of 6 ints, ranging from 1 - 49
+// 4. Any function that ensures no duplicate on the results
