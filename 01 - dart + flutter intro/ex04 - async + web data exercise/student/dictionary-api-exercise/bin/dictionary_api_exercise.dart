b5056319b970ffe1a62ac9a41e6b5e91b8566a60
Juan Lugo <jlugohernandez1@nait.ca>
2026-09-11 11:26:20 -0600

    Added the exercise 4 setup

---
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api-exercise/bin/dictionary_api_exercise.dart
@@ -0,0 +1,5 @@
+import 'package:dictionary_api_exercise/dictionary_api_exercise.dart' as dictionary_api_exercise;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dictionary_api_exercise.calculate()}!');
+}
---
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api-exercise/test/dictionary_api_exercise_test.dart
@@ -0,0 +1,8 @@
+import 'package:dictionary_api_exercise/dictionary_api_exercise.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
