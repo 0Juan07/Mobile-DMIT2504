f425d93850ba7165c5b74aa85355fe73ca114436
Juan Lugo <jlugohernandez1@nait.ca>
2026-09-15 14:43:53 -0600

    Added my version along teach version for ex-04

--- 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api-exercise/bin/dictionary_api_exercise.dart
+++ 01 - dart + flutter intro/ex04 - async + web data exercise/student/dictionary-api-exercise/bin/dictionary_api_exercise.dart
@@ -1,5 +1,28 @@
-import 'package:dictionary_api_exercise/dictionary_api_exercise.dart' as dictionary_api_exercise;
+// 1. Get user input for a word to lookup
+// 2. I need a function that:
+//        - attach that value to the end of a URL
+//        - send a get request at that URL
+//        - tries to return the JSON-decoded body of that
+// 3. Loop this until the user quits
+// 4. Create a class to represent the returned word
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dictionary_api_exercise.calculate()}!');
+import 'package:dictionary_api_exercise/http_requests.dart';
+
+void main() async {
+  // MINE Version
+  // stdout.write("Enter the word to lookup: ");
+  // String word = stdin.readLineSync()!;
+
+  // var wordData = await getJSON(API_WORKING_BASE_URL, word);
+
+  // print('Word: ${wordData['word']}');
+
+  // Teach
+  print("What word want to look up? ");
+  String word = stdin.readLineSync()!;
+
+  final definition = await getDefinition(word);
+
+  print('This is the $definition');
 }
